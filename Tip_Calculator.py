af0567161ed109df3bb3afaa632437be2fe7ce2d
Bhargavi Saikia <bhargavisaikia187@gmail.com>
2021-04-15 23:39:44 +0530

    add percentage.bill amount and it's entry using Label and Entry widgets

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -6,7 +6,19 @@ class TipCal():
         window.title("Tip Calculator")
         window.geometry("380x300")
         window.resizable(width=False, height= False)
-        window.configure(background="light blue")
+        window.configure(background="SteelBlue4")
+
+        tip_percentage=Label(window, text="Tip Percentages", bg="yellow", fg="black", width=15)
+        tip_percentage.grid(row=0, column=0, padx=14, pady=14)
+
+        bill_amount=Label(window, text="Bill_amount", bg="cyan2", fg="black", width=15)
+        bill_amount.grid(row=0, column=1, padx=10)
+
+        self.mealcost=StringVar()
+        bill_amount_entry=Entry(window, textvariable=self.mealcost, width=14)
+        bill_amount_entry.grid(row=0, column=3, padx=7)
+
         window.mainloop()
+
 
 TipCal()
