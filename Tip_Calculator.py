ca2b8bc82f5327da4b05db2c2986786b0fa2e095
Bhargavi Saikia <bhargavisaikia187@gmail.com>
2021-04-16 20:31:50 +0530

    rearrange fields, add calculate and clear buttons using button widget

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -16,7 +16,7 @@ class TipCal():
 
         self.mealcost = StringVar()
         bill_amount_entry = Entry(window, textvariable = self.mealcost, width = 14)
-        bill_amount_entry.grid(row = 0, column = 3, padx = 7)
+        bill_amount_entry.grid(row = 0, column = 2, padx = 7)
 
         self.tippercent = IntVar()
         five_percent = Radiobutton(window, text = "5%", variable = self.tippercent, value = 5 )
@@ -38,18 +38,25 @@ class TipCal():
         thirty_percent.grid(row = 6, column = 0)
 
         tip_amount_lbl = Label(window, text="Tip Amount", bg = "wheat1", fg = "black", width = 15)
-        tip_amount_lbl.grid(row = 3, column = 1, padx = 10 )
+        tip_amount_lbl.grid(row = 2, column = 1, padx = 10 )
 
         self.tipamount = StringVar()
         tip_amount_entry = Entry(window, textvariable=self.tipamount, width=14)
-        tip_amount_entry.grid(row=3, column=3, padx=7)
+        tip_amount_entry.grid(row = 2, column = 2, padx = 7)
 
         bill_total_lbl = Label(window, text = "Bill Total", bg = "IndianRed1", fg = "black", width = 15)
-        bill_total_lbl.grid(row = 6, column = 1, padx = 7 )
+        bill_total_lbl.grid(row = 5, column = 1, padx = 7 )
 
         self.billtotal = StringVar()
         bill_total_entry = Entry(window, textvariable = self.billtotal, width = 14)
-        bill_total_entry.grid(row = 6, column = 3, padx = 7)
+        bill_total_entry.grid(row = 5, column = 2, padx = 7)
+
+        calculate_button = Button(window, text = "Calculate", bg = "grey36", fg = "black", width = 10, command = "")
+        calculate_button.grid(row = 7, column = 1, padx = 15, pady = 10)
+
+        clear_button = Button(window, text = "Clear", bg = "thistle", fg = "black", width = 10, command = "")
+        clear_button.grid(row = 7, column = 2, padx = 15, pady = 10)
+
 
         window.mainloop()
 
